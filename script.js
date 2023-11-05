842647df3ca2f58e38e981ca2c5727d3ab85bc22
Ritik Upadhyay <116427440+ritiku2004@users.noreply.github.com>
2023-11-05 14:09:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
+faqHeaders.forEach((header,i)=>{
+    header.addEventListener("click",()=>{
+        header.nextElementSibling.classList.toggle("active");
+    });
+});
